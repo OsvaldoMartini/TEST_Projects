dfb0e0ad463e6f73f5a29abb51493060c5eafa47
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-04-08 14:02:22 +0100

    DataGrid Properties

--- WPF_Project/Binding_StaticResource_AddNew/MainWindow.xaml.cs
+++ WPF_Project/Binding_StaticResource_AddNew/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows;
     using Binding.StaticResource.AddNew.Model;
 using Binding.StaticResource.AddNew.ViewModel;
@@ -35,6 +37,7 @@ namespace Binding.StaticResource.AddNew
 
             SetUserModule_ViewModel();
 
+            GridAuthors.ItemsSource = LoadCollectionData();
         }
 
         private void SetUserModule_ViewModel()
@@ -43,7 +46,7 @@ namespace Binding.StaticResource.AddNew
             user.UserName = "User";
             user.IsModified = false;
             //DataContext = user;
-            this.GridUserViewModel.DataContext = user;
+            this.GridUserViewModel.DataContext = user;  
         }
 
         private void SettingObservableEvents()
@@ -223,6 +226,44 @@ namespace Binding.StaticResource.AddNew
         #endregion
 
 
+        /// List of Authors
+        /// </summary>
+        /// <returns></returns>
+        private List<Author> LoadCollectionData()
+        {
+            List<Author> authors = new List<Author>();
+            authors.Add(new Author()
+            {
+                ID = 101,
+                Name = "Mahesh Chand",
+                BookTitle = "Graphics Programming with GDI+",
+                DOB = new DateTime(1975, 2, 23),
+                IsMVP = false
+            });
+            authors.Add(new Author()
+            {
+                ID = 201,
+                Name = "Mike Gold",
+                BookTitle = "Programming C#",
+                DOB = new DateTime(1982, 4, 12),
+                IsMVP = true
+            });
+            authors.Add(new Author()
+            {
+                ID = 244,
+                Name = "Mathew Cochran",
+                BookTitle = "LINQ in Vista",
+                DOB = new DateTime(1985, 9, 11),
+                IsMVP = true
+            });
+            return authors;
+        }
+
+        private void RowColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            Author author = (Author)GridAuthors.SelectedItem;
+            MessageBox.Show("Selected author: " + author.Name);
+        } 
     }
 
 }
--- WPF_Project/Binding_StaticResource_AddNew/Model/Customer.cs
+++ WPF_Project/Binding_StaticResource_AddNew/Model/Customer.cs
@@ -1,9 +1,12 @@
-﻿namespace Binding.StaticResource.AddNew.Model
+﻿using System;
+
+namespace Binding.StaticResource.AddNew.Model
 {
     public class Customer
     {
         public int customerID {get; set; }
         public string name { get; set; }
+        public DateTime Date { get; set; }
     }
 
 }
